80f4fea80689fa2fe9ed5d4f82d271c8a4cf7ada
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-03 15:39:40 +0200

    feat: add first inputs to create budget page

--- src/components/pages/create-budget/CreateBudget.tsx
+++ src/components/pages/create-budget/CreateBudget.tsx
@@ -1,14 +1,52 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import { Box, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import React, { useState } from 'react';
 
 import Container from '@/components/shared/container/Container';
 import Navigation from '@/components/shared/navigation/Navigation';
 
+import classes from './CreateBudget.module.scss';
+
 const CreateBudget: React.FC = () => {
+  const [value, setValue] = useState('');
+
+  const dateChangeHandler = (newValue: unknown): void => {
+    setValue(newValue);
+  };
+
   return (
     <Box>
       <Navigation title="Create budget" />
-      <Container></Container>
+      <Container>
+        <Box component="form" className={classes.form}>
+          <Typography variant="h4" className={classes.title}>
+            Create budget 💰
+          </Typography>
+
+          <LocalizationProvider dateAdapter={AdapterLuxon}>
+            <DatePicker
+              label="Month"
+              views={['month']}
+              value={value}
+              onChange={dateChangeHandler}
+              renderInput={(params) => <TextField {...params} />}
+              className={classes.input}
+            />
+          </LocalizationProvider>
+
+          <FormControl fullWidth>
+            <InputLabel id="currency-input">Currency</InputLabel>
+            <Select labelId="currency-input" label="Currency">
+              <MenuItem value="UAH">UAH</MenuItem>
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="EUR">EUR</MenuItem>
+            </Select>
+          </FormControl>
+
+          <TextField label="Sum" type="number" className={classes.input} />
+        </Box>
+      </Container>
     </Box>
   );
 };
