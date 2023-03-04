d8c6b89ab5c03bcb5aa396297715951cbf3fec29
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-04 21:12:04 +0200

    feat: add base settings form

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,25 +1,39 @@
-import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import React from 'react';
+import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import React, { FormEvent, useState } from 'react';
 
 import Container from '@/components/shared/container/Container';
 import Navigation from '@/components/shared/navigation/Navigation';
-import { CURRENCY_ARRAY } from '@/constants/currency.constant';
+import { Currency, CURRENCY_ARRAY } from '@/constants/currency.constant';
 
 const SettingsPage: React.FC = () => {
+  const [currencyValue, setCurrencyValue] = useState<Currency | undefined>();
+
+  const currencyChangeHandler = (event: SelectChangeEvent<Currency>): void => {
+    const newValue = event.target.value as Currency;
+
+    setCurrencyValue(newValue);
+  };
+
+  const formSubmitHandler = (event: FormEvent): void => {
+    event.preventDefault();
+  };
+
   return (
     <Box>
       <Navigation />
       <Container>
-        <FormControl fullWidth>
-          <InputLabel id="currency-input">Currency</InputLabel>
-          <Select labelId="currency-input" label="Currency">
-            {CURRENCY_ARRAY.map((currency) => (
-              <MenuItem value={currency} key={currency}>
-                {currency}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+        <Box component="form" onSubmit={formSubmitHandler}>
+          <FormControl fullWidth>
+            <InputLabel id="currency-input">Currency</InputLabel>
+            <Select labelId="currency-input" label="Currency" value={currencyValue} onChange={currencyChangeHandler}>
+              {CURRENCY_ARRAY.map((currency) => (
+                <MenuItem value={currency} key={currency}>
+                  {currency}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
       </Container>
     </Box>
   );
