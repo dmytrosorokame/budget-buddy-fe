0e0542a2a714330d28e447db26b913c06c137d35
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-04 19:48:03 +0200

    feat: add budgets api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,15 @@
 import AuthApi from './auth/auth.api';
+import { BudgetsApi } from './budgets/budgets.api';
 
 class Api {
   public authApi: AuthApi;
+  public budgetsApi: BudgetsApi;
 
   private apiURL = 'http://localhost:3001';
 
   constructor() {
     this.authApi = new AuthApi(this.apiURL);
+    this.budgetsApi = new BudgetsApi(this.apiURL);
   }
 }
 
---
+++ src/api/budgets/budgets.api.ts
@@ -0,0 +1,19 @@
+import { AxiosInstance } from 'axios';
+
+import getAxiosInstance from 'api/axios';
+
+import { IBudget } from './../../types/budgets.types';
+
+export class BudgetsApi {
+  private axios: AxiosInstance;
+
+  constructor(private apiUrl: string) {
+    this.axios = getAxiosInstance();
+  }
+
+  public async getAll(): Promise<IBudget[]> {
+    const response = await this.axios.get(`${this.apiUrl}/budgets`);
+
+    return response.data;
+  }
+}
