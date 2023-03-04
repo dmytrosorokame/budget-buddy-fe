7bc478988ea1831966021829428222915fe38509
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-04 21:06:45 +0200

    feat: add dynamic currency select

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,12 +1,26 @@
-import { Box } from '@mui/material';
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 import React from 'react';
 
+import Container from '@/components/shared/container/Container';
 import Navigation from '@/components/shared/navigation/Navigation';
+import { CURRENCY_ARRAY } from '@/constants/currency.constant';
 
 const SettingsPage: React.FC = () => {
   return (
     <Box>
       <Navigation />
+      <Container>
+        <FormControl fullWidth>
+          <InputLabel id="currency-input">Currency</InputLabel>
+          <Select labelId="currency-input" label="Currency">
+            {CURRENCY_ARRAY.map((currency) => (
+              <MenuItem value={currency} key={currency}>
+                {currency}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Container>
     </Box>
   );
 };
