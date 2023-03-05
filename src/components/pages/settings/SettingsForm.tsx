02321acbec3bd56e4adcb61a7fb09ea8ca2c73e5
Dmytro Soroka <dmytro.soroka@betterme.world>
2023-03-05 10:43:17 +0200

    feat: add settings logic & improve styles

---
+++ src/components/pages/settings/SettingsForm.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Typography } from '@mui/material';
+import React, { FormEvent, useEffect, useState } from 'react';
+
+import { Currency, CURRENCY_ARRAY } from '@/constants/currency.constant';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { selectUserCurrency } from '@/redux/user/user.selectors';
+import { getMe, updateUser } from '@/redux/user/user.thunks';
+
+import classes from './SettingsForm.module.scss';
+
+const SettingsForm: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const [currencyValue, setCurrencyValue] = useState<Currency | undefined>(Currency.USD);
+
+  const userCurrency = useAppSelector(selectUserCurrency);
+
+  const currencyChangeHandler = (event: SelectChangeEvent<Currency>): void => {
+    const newValue = event.target.value as Currency;
+
+    setCurrencyValue(newValue);
+  };
+
+  const formSubmitHandler = (event: FormEvent): void => {
+    event.preventDefault();
+
+    dispatch(updateUser({ currency: currencyValue }));
+  };
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    setCurrencyValue(userCurrency);
+  }, [userCurrency]);
+
+  return (
+    <Box component="form" onSubmit={formSubmitHandler} className={classes.form}>
+      <Typography className={classes.title} variant="h4">
+        Your settings ⚙️
+      </Typography>
+      <FormControl fullWidth>
+        <InputLabel id="currency-input">Currency</InputLabel>
+        <Select labelId="currency-input" label="Currency" value={currencyValue} onChange={currencyChangeHandler}>
+          {CURRENCY_ARRAY.map((currency) => (
+            <MenuItem value={currency} key={currency}>
+              {currency}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button type="submit" variant="contained" className={classes.button}>
+        Save
+      </Button>
+    </Box>
+  );
+};
+
+export default SettingsForm;
--- src/redux/user/user.thunks.ts
+++ src/redux/user/user.thunks.ts
@@ -3,9 +3,27 @@ import { AxiosError } from 'axios';
 
 import api from '@/api/api';
 
+import { IUser } from './../../types/user.types';
+
 export const getMe = createAsyncThunk('user/getMe', async (n, { rejectWithValue }) => {
   try {
     const response = await api.userApi.getMe();
+
+    return response;
+  } catch (error: unknown) {
+    const typedError = error as AxiosError;
+
+    if (!typedError.response) {
+      throw typedError;
+    }
+
+    return rejectWithValue(typedError.response.data);
+  }
+});
+
+export const updateUser = createAsyncThunk('user/updateUser', async (payload: Partial<IUser>, { rejectWithValue }) => {
+  try {
+    const response = await api.userApi.updateUser(payload);
 
     return response;
   } catch (error: unknown) {
